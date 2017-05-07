cad007820c9eab26247c1bea28e869d130e4fca2
Grzegorz <apps@getthatapp.pl>
2017-05-07 15:59:58 +0200

    Fixed BoardsController in create action and ListsController in create and update actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,7 +23,7 @@ class ListsController < ApplicationController
 
   def update
     if list.update(list_params)
-      redirect_to board_path(board, @list), notice: 'List updated'
+      redirect_to board_path(board, list), notice: 'List updated'
     else
       render :edit
     end
