b0e460863dcf37e8d41ddf363d5ba0f2974e176e
Grzegorz <apps@getthatapp.pl>
2017-05-10 07:58:22 +0200

    Create: task model, tasks controller, tasks table, migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505070729) do
+ActiveRecord::Schema.define(version: 20170510055020) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20170505070729) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "list_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
