8208d045071903475c181ce033c4ce0fb8d1d3f0
Grzegorz <apps@getthatapp.pl>
2017-05-31 16:57:47 +0200

    Add: login, logout ability and header links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.password == params[:password]
-      session[:current_user_id] = user.current_user_id
+      session[:current_user_id] = user.id
       redirect_to root_path
     else
       flash.now.alert = "Invalid email or password"
