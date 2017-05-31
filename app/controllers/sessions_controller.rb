0561f3c1dcad12d68275f5dd6ab3974b50d92e57
Grzegorz <apps@getthatapp.pl>
2017-05-31 16:14:55 +0200

    Add: bcrypt functions, registration ability, sessions, user validations and password actions, change forms in registration_new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
-  def new; end
-  def create; end
-  def destroy; end
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.password == params[:password]
+      session[:current_user_id] = user.current_user_id
+      redirect_to root_path
+    else
+      flash.now.alert = "Invalid email or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
-  has_secure_password
+  include BCrypt
+  # has_secure_password
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
-  validates :password, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
