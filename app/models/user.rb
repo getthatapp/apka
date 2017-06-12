4fff8845c95987aa613038ad18bcc09b7bb7c7e4
Grzegorz <apps@getthatapp.pl>
2017-06-12 19:28:59 +0200

    Add users table, user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+
+  has_many :boards, dependent: :destroy
+  
   include BCrypt
   has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 
   # def password
   #   @password ||= Password.new(password_digest)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607095254) do
+ActiveRecord::Schema.define(version: 20170612172015) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170607095254) do
     t.integer  "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "password_confirmation"
   end
 
 end
