2100049f3a18de3ebcaf1465083b4ab93a4744e4
Grzegorz <apps@getthatapp.pl>
2017-06-12 19:03:50 +0200

    Fixed User model, application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
-
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = User.find_by(id: session[:current_user_id])
   end
+  helper_method :current_user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
   include BCrypt
-  # has_secure_password
+  has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
-  def password
-    @password ||= Password.new(password_digest)
-  end
+  # def password
+  #   @password ||= Password.new(password_digest)
+  # end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_digest = @password
-  end
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_digest = @password
+  # end
 end
